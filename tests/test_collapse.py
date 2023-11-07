492b98261a9c4ef6855c136a37d79f2372e179fe
Jack Tierney <jack.tierney@student.ucc.ie>
2023-11-07 16:22:01 +0000

    updated collapse tests to cover errors

--- tests/test_collapse.py
+++ tests/test_collapse.py
@@ -3,14 +3,15 @@ from RDPTools.collapse import collapse
 
 import gzip
 import hashlib
+import pytest
 
 
 def test_collapse_fq():
     collapse(
         'tests/test_data/reads.fq',
-        'tests/test_data/reads.collapsed.fq',
+        'tests/test_data/reads.collapsed.fa',
     )
-    with open('tests/test_data/reads.collapsed.fq', 'rb') as f:
+    with open('tests/test_data/reads.collapsed.fa', 'rb') as f:
         m = hashlib.md5()
         m.update(f.read())
         assert m.hexdigest() == '1d2cc643a8ca7642e68964fae580b56b'
@@ -18,9 +19,8 @@ def test_collapse_fq():
 def test_collapse_fq_gz():
     collapse(
         'tests/test_data/reads.fq.gz',
-        'tests/test_data/reads.collapsed.fq.gz',
+        'tests/test_data/reads.collapsed.fa.gz',
     )
-    
     # these files must be read differently as gzip files from python automatically
     # add the metadata (no -n flag) so MD5 hashes will not match
     with gzip.open('tests/test_data/reads.collapsed.fq.gz', 'rb') as f:
@@ -32,11 +32,43 @@ def test_collapse_fq_gz():
 def test_collapse_format():
     collapse(
         'tests/test_data/reads.fq',
-        'tests/test_data/reads.collapsed.fq',
-        format='>testREADtestCOUNT',
+        'tests/test_data/reads.collapsed.fa',
+        format='>testREAD_xCOUNT',
     )
     with open('tests/test_data/reads.collapsed.fq', 'rb') as f:
         m = hashlib.md5()
         m.update(f.read())
         assert m.hexdigest() == 'd05b84c78663ef7b5455561bae4de704'
 
+def test_collapse_error_raised_unqi_delims():
+    with pytest.raises(ValueError, match="read and count must have unique delimiters"):
+        collapse(
+            'tests/test_data/reads.fq',
+            'tests/test_data/reads.collapsed.fa',
+            format='>testREADtestCOUNT',
+        )
+        with open('tests/test_data/reads.collapsed.fq', 'rb') as f:
+            m = hashlib.md5()
+            m.update(f.read())
+
+def test_collapse_error_raised_missing_delim():
+    with pytest.raises(ValueError, match="Template must contain READ and COUNT placeholders"):
+        collapse(
+            'tests/test_data/reads.fq',
+            'tests/test_data/reads.collapsed.fa',
+            format='>testtestCOUNT',
+        )
+        with open('tests/test_data/reads.collapsed.fq', 'rb') as f:
+            m = hashlib.md5()
+            m.update(f.read())
+
+def test_collapse_error_raised_double_read():
+    with pytest.raises(ValueError, match="Template must contain only one READ and COUNT placeholder"):
+        collapse(
+            'tests/test_data/reads.fq',
+            'tests/test_data/reads.collapsed.fa',
+            format='>testREADtestCOUNT_READ',
+        )
+        with open('tests/test_data/reads.collapsed.fq', 'rb') as f:
+            m = hashlib.md5()
+            m.update(f.read())
